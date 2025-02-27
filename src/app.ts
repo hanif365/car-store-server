09b03a9531aa519eaa7d49037af843e1fdc08e45
hanif365 <m.a.hanifkhan64@gmail.com>
2025-02-27 10:14:47 +0600

    deploy in vercel

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://car-store-client-ten.vercel.app'],
     credentials: true,
   }),
 );
